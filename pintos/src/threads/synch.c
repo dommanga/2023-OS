0cd65a17cc8fe8eed907c006837f3888cafdacff
dommanga <leejm21@postech.ac.kr>
2023-10-05 22:31:56 +0900

    fix bug with {} when add list_sort in synch.c haha

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -114,9 +114,10 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    list_sort(&sema->waiters, thread_comp_priority, NULL);
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+    {
+      list_sort(&sema->waiters, thread_comp_priority, NULL);
+      thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
+    }
   sema->value++;
   intr_set_level (old_level);
 }
@@ -397,9 +398,10 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (lock_held_by_current_thread (lock));
 
   if (!list_empty (&cond->waiters)) 
+  {
     list_sort(&cond->waiters, sema_comp_priority, NULL);
-    sema_up (&list_entry (list_pop_front (&cond->waiters),
-                          struct semaphore_elem, elem)->semaphore);
+    sema_up (&list_entry (list_pop_front (&cond->waiters), struct semaphore_elem, elem)->semaphore);
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
