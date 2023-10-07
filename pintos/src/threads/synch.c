f8391d61d73f5afa3a8f96b3168e4b38280b10f6
ldm0902 <ldm0902@postech.ac.kr>
2023-10-07 13:39:13 +0900

    update synch.c to avoid priority donation

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -201,6 +201,12 @@ lock_acquire (struct lock *lock)
 
   struct thread *cur = thread_current();
 
+  if(thread_mlfqs==true){
+    sema_down (&lock->semaphore);
+    lock->holder = cur;
+    return;
+  }
+
   if (lock->holder != NULL)
   { 
     cur->wait_lock = lock;
@@ -257,6 +263,12 @@ lock_release (struct lock *lock)
 {
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
+
+  lock->holder = NULL;
+  if(thread_mlfqs==true){
+    sema_up (&lock->semaphore);
+    return;
+  }
 
   del_lock_donators(lock);
   re_set_effective_priority();
