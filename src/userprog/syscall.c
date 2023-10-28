17f7fd54c9a23357d28acabecf967fdce6e59ac0
dommanga <leejm21@postech.ac.kr>
2023-10-28 10:41:42 +0900

    organize syscall_handler func

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,23 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "lib/user/syscall.h"
 
 static void syscall_handler (struct intr_frame *);
+void halt (void);
+void exit (int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
 
 void
 syscall_init (void) 
@@ -15,6 +30,99 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int syscall_num = *(uint32_t *)f->esp;
+
+ switch (syscall_num)
+ {
+  case SYS_HALT:
+    break;
+  case SYS_EXIT:
+    break;
+  case SYS_EXEC:
+    break;
+  case SYS_WAIT:
+    break;
+  case SYS_CREATE:
+    break;
+  case SYS_REMOVE:
+    break;
+  case SYS_OPEN:
+    break;
+  case SYS_FILESIZE:
+    break;
+  case SYS_READ:
+    break;
+  case SYS_WRITE:
+    break;
+  case SYS_SEEK:
+    break;
+  case SYS_TELL:
+    break;
+  case SYS_CLOSE:
+    break;
+ } 
+}
+
+void halt (void)
+{
+
+}
+
+void exit (int status)
+{
+
+}
+
+pid_t exec (const char *cmd_line)
+{
+
+}
+
+int wait (pid_t pid)
+{
+
+}
+
+bool create (const char *file, unsigned initial_size)
+{
+
+}
+
+bool remove (const char *file)
+{
+
+}
+
+int open (const char *file)
+{
+
+}
+
+int filesize (int fd)
+{
+
+}
+
+int read (int fd, void *buffer, unsigned size)
+{
+
+}
+
+int write (int fd, const void *buffer, unsigned size)
+{
+
+}
+
+void seek (int fd, unsigned position)
+{
+
+}
+unsigned tell (int fd)
+{
+
+}
+
+void close (int fd)
+{
+
 }
