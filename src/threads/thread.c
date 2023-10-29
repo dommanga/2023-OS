c942fa88ee39461cea2f862760220efe528e12a8
dommanga <leejm21@postech.ac.kr>
2023-10-29 19:46:02 +0900

    initialize fd_idx to 2 also.

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -494,6 +494,7 @@ init_thread (struct thread *t, const char *name, int priority)
     list_push_back(&running_thread()->child_list, &t->child_elem);
     for (int i = 0; i < 128; i++)
       t->fdt[i] = NULL;
+    fd_idx = 2; //0, 1 is std I/O
   #endif
 
   t->magic = THREAD_MAGIC;
